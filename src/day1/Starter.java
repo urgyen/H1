fac5d42bcd9e337ecfeee748883a730cc8b90808
Urgyen Shakya <urgyenshakya@gmail.com>
2020-05-12 10:14:43 +0545

    may12

---
+++ src/day1/Starter.java
@@ -0,0 +1,200 @@
+package day1;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Good Morning From Nepal ");
+		//Variables
+		// x ko vary hunxa
+		
+//		int x = 11;
+//		String name="Apple";
+//		String age= "25";
+//		//age-5;
+//		
+//		
+//		System.out.println(age);
+//		
+//		//Parsing
+//		//From one data type to another
+//		int ageInInt= Integer.parseInt(age);
+//		System.out.println(ageInInt-5);
+//		
+//		double height = 5.8;
+//		int heightinINT = Integer.parseInt(height);
+//		
+		//Spillage
+		
+//		int phoneNo =123;
+//		double phoneInDouble= phoneNo;
+//		int height1= 5.8;
+//		System.out.println(heightInt);
+		
+//		
+//		//Naming Convention
+//		
+//		String name;
+//		//CAmelCAsing?
+//		//upper CAmel Casing
+//		//Lower Camel CAsing
+//		
+//		String firstName= "Obama";
+//		String LastName ="Trump";
+//		
+//		
+//		//variables lower CAmel Casing//
+//		
+//		
+//		String _name= "A";
+//		String $address="";
+//		
+//		String phone= "";
+//		
+//		//Datatype??
+//		//Primitive 
+//		//Non Primitive
+//		
+//		//age= int;
+//		
+//		//ASCII
+//		String  name= "ASDASDSAD";
+//		//??? 16 bit -- > 4 byte?		
+//		// 2 byte-- 8 bits 
+//		 char a= 'a';
+//		 
+//		 boolean isTRue == true or 	false?
+//				 ```		1		0
+		 
+		//Operators?
+//				 
+//				int a1=7;
+//				int b1=3;
+//				
+//				int a= a1+b1;		 
+//				System.out.println(a);
+//				
+//				
+//				int a=17;
+//				int b=8;
+//				
+//				System.out.println(a%b);
+		
+		
+		//ternary Operator?
+			// int tocheck= 15;
+			 //enhanced if else??
+			 
+			 //return type;
+			 
+		//	 String isTrue = ((tocheck %2 )==0 ) ? 'Even' : 'Odd' ;
+			 
+//			 int num1=10;
+//			 int num2=15;
+//			// 					 ( condition ) false ? if True  : if false ;
+//			 int biggerNumber= (num1 > num2) ?  num1 : num2 ;
+//			 
+//			 if(num1>num2) {
+//				 System.out.println(num1);
+//			 }
+//			 else {
+//				 System.out.println(num2);
+//			 }
+//			 
+//			 System.out.println(biggerNumber);
+//			 
+			 //System.out.println(isTrue);
+				
+			 //Unary Operators
+			 //a++; a--, ++aa; --a;
+			 //a++;
+			 //a--;
+			 
+//			 int a=1;
+//			 a++;
+//			 //a=a+1;
+//			 a++;
+//			 System.out.println(a);
+		
+		//Looping ko lagi auto increment
+			
+		//Java ma chai 3 types??
+				
+		//1. Lets do something
+			//Statemsnts
+		
+		
+		//2. Lets do if that happens
+			//DEcision Cotrol
+				//1 . if else
+					
+		
+		//India == "Modi"
+		//Nepal  == Kp baa
+		//Nepali == Wrong Input???
+//		
+//		String country ="Nepal";
+//		System.out.println(country);
+//		
+//					if( country=="USA" ) {
+//						System.out.println("Trump");
+//					}
+//					
+//					else if(country=="India") {
+//						System.out.println("Modi");
+//					}
+//					
+//					else if(country =="Nepal") {
+//						System.out.println("Oli baa");
+//					}
+//					else {
+//						System.out.println(" ____ Wrong Input");
+//					}
+					
+				//2. else if
+				
+					//3. switch case
+						//research garnus?
+		
+		//3. Lets do that again and again 
+					//Loop
+					
+					//do while
+					//while
+					//for 
+					//for each  -->> paxi
+		
+//					int i =1;
+//					
+//					do {
+//						System.out.println( i+ " hEllo");
+//						i++;
+//					}while(i<=5);
+//		
+//				System.out.println("VAlue of i is " + i);
+//					
+//				i=1;
+//				
+//					while(i<=5) {
+//						System.out.println("Hi " + i);
+//						i++;
+//					}
+		//	int j=0;
+		//	initialization
+		//	condition
+		//	increment
+		
+		//odd number between 1 and 200
+		for(int j=1; j<=200 ;  j=j+2) {
+			System.out.println(j);
+		}
+		
+		//exit
+		
+		//DEcision Control 
+		
+					
+	}
+
+}
